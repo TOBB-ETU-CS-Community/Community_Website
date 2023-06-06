6ad2e68ad99a480c8f0ec2e681b97c1bd8ab8d43
fotino21 <ataturhan21@gmail.com>
2023-06-06 19:25:18 +0300

    feature: etkinlik takvimi page improved

--- community_website/pages/2_📅Etkinlik Takvimimiz.py
+++ community_website/pages/2_📅Etkinlik Takvimimiz.py
@@ -1,67 +1,80 @@
-from datetime import date
+from datetime import date, timedelta
 
 import pandas as pd
 import streamlit as st
 from calendar_view.calendar import Calendar
+from calendar_view.config import style
 from calendar_view.core import data
 from calendar_view.core.config import CalendarConfig
 from calendar_view.core.event import Event, EventStyles
 from modules.configurations import add_bg_from_local
 
 
-def show_calendar():
-    config = CalendarConfig(
-        lang="en",
-        title="TOBB ETU Bilgisayar Topluluğı Geçmiş Etkinlik Takvimi",
-        dates="Mo - Su",
-        hours="12 - 22",
-        show_date=False,
-        legend=False,
+def load_calendar_excel():
+    calendar = pd.read_excel(
+        "input/Etkinlik Takvimi.xlsx",
+        sheet_name="Sheet1",
     )
-    data.validate_config(config)
+    return calendar
 
-    events = [
-        Event(
-            day_of_week=1,
-            start="18:00",
-            end="19:15",
-            title="HOT Core Yoga, 75 mins, with David",
-            style=EventStyles.RED,
-        ),
-        Event(
-            day_of_week=2,
-            start="19:00",
-            end="20:15",
-            title="Hatha Yoga, 75 mins, with Jo",
-            style=EventStyles.GREEN,
-        ),
-        Event(
-            day_of_week=3,
-            start="19:00",
-            end="21:00",
-            title="Pilates, 60 mins, with Erika",
-            style=EventStyles.BLUE,
-        ),
-        Event(
-            day_of_week=4,
-            start="18:30",
-            end="20:00",
-            title="Kundalini Yoga, 90 mins, with Dan",
-            style=EventStyles.RED,
-        ),
-        Event(
-            day_of_week=6,
-            start="14:00",
-            end="15:15",
-            title="Hatha Yoga, 75 mins, with Vick",
-            style=EventStyles.GREEN,
-        ),
-    ]
 
-    calendar = Calendar.build(config)
-    calendar.add_events(events)
-    calendar.save("takvim.png")
-    st.image("takvim.png")
+def show_calendar(calendar, start_date, end_date):
+    start_date = start_date - timedelta(days=start_date.weekday())
+
+    end_date = end_date - timedelta(days=end_date.weekday())
+    end_date = end_date + timedelta(days=6)
+
+    calendar["Tarih"] = pd.to_datetime(calendar["Tarih"]).dt.date
+
+    style.hour_height = 80
+    current_date = start_date
+    while current_date < end_date:
+        end_of_week = current_date + timedelta(days=6)
+
+        selected_calendar = calendar[
+            (current_date <= calendar["Tarih"])
+            & (calendar["Tarih"] <= end_of_week)
+        ]
+        if len(selected_calendar) > 0:
+            config = CalendarConfig(
+                lang="en",
+                title="TOBB ETU Bilgisayar Topluluğı Geçmiş Etkinlik Takvimi",
+                dates=f"{current_date} - {end_of_week}",
+                hours="12 - 22",
+                show_date=True,
+                show_year=True,
+                legend=False,
+                title_vertical_align="center",
+            )
+            data.validate_config(config)
+
+            events = []
+
+            for i in range(len(selected_calendar)):
+                row = selected_calendar.iloc[i]
+                color = EventStyles.GRAY
+                if row["Düzenleyen"] == "Blockchain Kulübü":
+                    color = EventStyles.BLUE
+                elif row["Düzenleyen"] == "Yapay Zeka Kulübü":
+                    color = EventStyles.GREEN
+                elif row["Düzenleyen"] == "Oyun Geliştirme Kulübü":
+                    color = EventStyles.RED
+
+                event = Event(
+                    day_of_week=row["Tarih"].weekday(),
+                    start=row["Başlangıç Saati"],
+                    end=row["Bitiş Saati"],
+                    title=row["İsim"] + f"\n({row['Düzenleyen']})",
+                    style=color,
+                )
+                events.append(event)
+
+            calendar_view = Calendar.build(config)
+            calendar_view.add_events(events)
+            calendar_view.save("takvim.png")
+            st.image("takvim.png")
+
+        current_date = current_date + timedelta(days=7)
 
 
 def main():
@@ -86,12 +99,12 @@ def main():
         unsafe_allow_html=True,
     )
     _, col1, col2, _ = st.columns([1, 2, 2, 1])
-    col1.date_input(
+    start_date = col1.date_input(
         "Tarih aralığının başlangıcını seçin:",
         min_value=date(2022, 3, 31),
         max_value=date.today() + pd.DateOffset(months=2),
     )
-    col2.date_input(
+    end_date = col2.date_input(
         "Tarih aralığının sonunu seçin:",
         min_value=date(2022, 3, 31),
         max_value=date.today() + pd.DateOffset(months=2),
@@ -110,7 +123,8 @@ def main():
     )
 
     if center_col.button("Takvimi Getir"):
-        show_calendar()
+        calendar = load_calendar_excel()
+        show_calendar(calendar, start_date, end_date)
 
 
 if __name__ == "__main__":
