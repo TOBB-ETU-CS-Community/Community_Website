45d478b95e8bcb1dfb6769fe644b724d07b1b657
fotino21 <ataturhan21@gmail.com>
2023-06-23 17:17:27 +0300

    refactor: configurations named to util

---
+++ community_website/modules/utils.py
@@ -0,0 +1,82 @@
+import base64
+from datetime import datetime
+
+import pandas as pd
+import streamlit as st
+
+
+def add_bg_from_local(background_file, sidebar_background_file):
+    with open(background_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    with open(sidebar_background_file, "rb") as image_file:
+        sidebar_encoded_string = base64.b64encode(image_file.read())
+
+    page = f"""<style>
+        .stApp {{
+            background-image: url(data:image/png;base64,{encoded_string.decode()});
+            background-size: 1500px 700px;
+        }}
+
+        section[data-testid="stSidebar"] {{
+            background-image: url(data:image/png;base64,{sidebar_encoded_string.decode()});
+            background-size: 400px 800px;
+        }}
+    </style>"""
+
+    st.markdown(page, unsafe_allow_html=True)
+
+
+def set_page_config():
+    st.set_page_config(
+        page_title="Bilgisayar Topluluğu",
+        page_icon="💻",
+        layout="wide",
+        initial_sidebar_state="expanded",
+        menu_items={
+            "Get Help": "https://github.com/TOBB-ETU-CS-Community",
+            "Report a bug": "https://tobbetu-bilgisayar-toplulugu.streamlit.app/Geri_Bildirim_Formu",
+            "About": "Topluluğumuza ait web sayfasında bize dair pek çok bilgiye ulaşabilirsiniz. \
+                Her türlü geri bildiriminize her zaman açığız.",
+        },
+    )
+
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+    style = """<style>
+        .row-widget.stButton {text-align: center;}
+        div[data-testid="column"]:nth-of-type(1)
+            {
+                text-align: center;
+            }
+            div[data-testid="column"]:nth-of-type(2)
+            {
+                text-align: center;
+            }
+            div[data-testid="column"]:nth-of-type(3)
+            {
+                text-align: center;
+            }
+    </style>"""
+    st.markdown(style, unsafe_allow_html=True)
+
+
+@st.cache_data
+def load_excel(
+    file: str,
+    date_column: str = None,
+    original_format: str = "%d.%m.%Y",
+    new_format: str = "%Y-%m-%d",
+):
+    excel = pd.read_excel(
+        file,
+        sheet_name="Sheet1",
+    )
+    if date_column is not None:
+        for i in range(len(excel)):
+            date_string = excel[date_column][i]
+            date_object = datetime.strptime(date_string, original_format)
+            new_date_string = date_object.strftime(new_format)
+            excel[date_column][i] = new_date_string
+    return excel
---
+++ community_website/pages/4_🌟 Gençlik Programları.py
@@ -0,0 +1,90 @@
+from datetime import date, datetime
+
+import pandas as pd
+import streamlit as st
+from modules.utils import add_bg_from_local, load_excel, set_page_config
+from PIL import Image
+
+
+@st.cache_data
+def get_open_programs(programs: pd.DataFrame, today: date):
+    open_programs = pd.DataFrame(columns=programs.columns)
+    for i in range(len(programs)):
+        if datetime.strptime(programs["Bitiş"][i], "%d-%m-%Y").date() >= today:
+            open_programs.loc[len(open_programs)] = programs.iloc[i]
+    open_programs.reset_index(drop=True, inplace=True)
+    return open_programs
+
+
+def main():
+    set_page_config()
+
+    add_bg_from_local(
+        background_file="input/Community Logo.png",
+        sidebar_background_file="input/Lila Gradient.png",
+    )
+
+    st.markdown(
+        """<h1 style='text-align: center; color: black; font-size: 40px;'> Üniversite öğrencilerine
+         yönelik gençlik programlarını aşağıda bulabilirsiniz. </h1>
+        <br>
+        """,
+        unsafe_allow_html=True,
+    )
+
+    programs = load_excel(
+        file="input/Programlar.xlsx",
+        date_column="Bitiş",
+        new_format="%d-%m-%Y",
+    )
+
+    programs.sort_values(by="Bitiş", inplace=True)
+    programs.reset_index(drop=True, inplace=True)
+
+    choice = st.sidebar.radio(
+        "Hangi programları görmek istersiniz?",
+        ("Açık Programlar", "Tüm Programlar"),
+    )
+    if choice == "Açık Programlar":
+        programs_to_show = get_open_programs(programs, today=date.today())
+    else:
+        programs_to_show = programs
+
+    for i in range(len(programs_to_show)):
+        _, center_col, _ = st.columns([1, 5, 1])
+        center_col.markdown(
+            f"""
+                <div style='text-align: center;  font-size: 40px;'>
+                <a href={programs_to_show['Link'][i]}>
+                {programs_to_show["İsim"][i]}
+                <br>
+                </a>
+                </div>
+                """,
+            unsafe_allow_html=True,
+        )
+        deadline = programs_to_show["Bitiş"][i]
+        center_col.markdown(
+            f"""
+                <div style='text-align: center;  font-size: 30px;'>
+                Son Başvuru Tarihi: {deadline}
+                <br>
+                <br>
+                </div>
+                """,
+            unsafe_allow_html=True,
+        )
+        image = Image.open(
+            f"input/program_images/{programs_to_show['İsim'][i]}.jpg"
+        )
+        center_col.image(image)
+        center_col.markdown(
+            f"""
+                <hr>
+                """,
+            unsafe_allow_html=True,
+        )
+
+
+if __name__ == "__main__":
+    main()
