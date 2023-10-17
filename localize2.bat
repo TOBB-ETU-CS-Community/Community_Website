c2e40c413f9997b9fa973b8fa33b96499b76c97c
fotino21 <ataturhan21@gmail.com>
2023-10-17 15:04:36 +0300

    feature: localizing app

---
+++ localize2.bat
@@ -0,0 +1,11 @@
+chcp 65001
+
+@echo
+
+poetry shell
+
+python C:\Users\NEO\AppData\Local\Programs\Python\Python39\Tools\i18n\msgfmt.py -o locales\en\LC_MESSAGES\base.mo locales\en\LC_MESSAGES\base
+
+python C:\Users\NEO\AppData\Local\Programs\Python\Python39\Tools\i18n\msgfmt.py -o locales\tr\LC_MESSAGES\base.mo locales\tr\LC_MESSAGES\base
+
+Exit
--- start.bat
+++ start.bat
@@ -5,3 +5,5 @@ chcp 65001
 poetry run streamlit run "C:\Users\NEO\Desktop\Projects\Community_Website\community_website\👋Hoşgeldiniz.py" --server.port 8501
 
 Exit
+
+poetry run streamlit run "C:\Users\NEO\Desktop\Projects\Community_Website\community_website\main.py" --server.port 8501
