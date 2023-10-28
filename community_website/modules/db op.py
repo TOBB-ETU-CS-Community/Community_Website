1ddd32aeede39ecba744541cbf68feebcd50df5a
fotino21 <ataturhan21@gmail.com>
2023-10-28 17:29:02 +0300

    feature: ui added for excel operations

--- community_website/modules/db op.py
+++ community_website/modules/db op.py
@@ -88,54 +88,75 @@ def main():
     )
     st.markdown(page_markdown, unsafe_allow_html=True)
 
-    st.markdown(
-        """<h1 style='text-align: center; color: black; font-size: 40px;'> Database
-        Operations </h1>
-        """,
-        unsafe_allow_html=True,
-    )
-
     with st.sidebar:
-        st.header("Current databases")
-        if st.button("Update databases"):
-            update_database()
-        show_tables_query = """
-        SELECT
-            name
-        FROM
-            sqlite_schema
-        WHERE
-            type ='table' AND
-            name NOT LIKE 'sqlite_%';
-        """
-        db_file = "cs_com_db.db"
-        conn = sqlite3.connect(db_file)
-        cur = conn.execute(show_tables_query)
-        if cur:
-            cols = [column[0] for column in cur.description]
-            results_df = pd.DataFrame.from_records(
-                data=cur.fetchall(), columns=cols
-            )
-            st.dataframe(results_df)
-
-    _, center_col, _ = st.columns([1, 3, 1])
-    query = center_col.text_area("SQL Query", height=150)
-
-    if center_col.button("Run Query"):
-        try:
+        st.selectbox(
+            "Choose the operation you want to do",
+            ("Database Operations", "Excel Operations"),
+            key="op_type",
+        )
+        if st.session_state.op_type == "Database Operations":
+            st.header("Current databases")
+            if st.button("Update databases"):
+                update_database()
+            show_tables_query = """
+            SELECT
+                name
+            FROM
+                sqlite_schema
+            WHERE
+                type ='table' AND
+                name NOT LIKE 'sqlite_%';
+            """
             db_file = "cs_com_db.db"
             conn = sqlite3.connect(db_file)
-            cur = conn.execute(query)
-            if "drop" not in query.lower():
+            cur = conn.execute(show_tables_query)
+            if cur:
                 cols = [column[0] for column in cur.description]
                 results_df = pd.DataFrame.from_records(
                     data=cur.fetchall(), columns=cols
                 )
-                center_col.dataframe(results_df)
-            center_col.success("Operation executed successfuly")
-        except Exception as e:
-            center_col.error(e)
-            return
+                st.dataframe(results_df)
+
+    if st.session_state.op_type == "Database Operations":
+        st.markdown(
+            """<h1 style='text-align: center; color: black; font-size: 40px;'> Database
+        Operations </h1>
+        """,
+            unsafe_allow_html=True,
+        )
+        _, center_col, _ = st.columns([1, 3, 1])
+        query = center_col.text_area("SQL Query", height=150)
+
+        if center_col.button("Run Query"):
+            try:
+                db_file = "cs_com_db.db"
+                conn = sqlite3.connect(db_file)
+                cur = conn.execute(query)
+                if "drop" not in query.lower():
+                    cols = [column[0] for column in cur.description]
+                    results_df = pd.DataFrame.from_records(
+                        data=cur.fetchall(), columns=cols
+                    )
+                    center_col.dataframe(results_df)
+                center_col.success("Operation executed successfuly")
+            except Exception as e:
+                center_col.error(e)
+                return
+    elif st.session_state.op_type == "Excel Operations":
+        st.markdown(
+            """<h1 style='text-align: center; color: black; font-size: 40px;'> Excel
+        Operations </h1>
+        """,
+            unsafe_allow_html=True,
+        )
+        _, center_col, _ = st.columns([1, 3, 1])
+        uploaded_file = center_col.file_uploader(
+            label="Upload the excel you want to translate",
+            type=["xlx", "xlsx"],
+        )
+        if uploaded_file is not None:
+            df = load_excel(uploaded_file)
+            st.dataframe(df)
 
 
 if __name__ == "__main__":
