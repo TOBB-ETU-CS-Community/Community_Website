999dcc6b3456d72fe33a358ff5898e9aeab12a1d
fotino21 <ataturhan21@gmail.com>
2023-11-06 17:15:50 +0300

    config: unittest added to precommit config yaml

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,16 +2,16 @@ import os
 import sys
 
 sys.path.append("../community_website")
-
 import unittest
 
 import pandas as pd
-from modules.utils import translate_excel
+
+from community_website.modules.utils import translate_excel
 
 
 class TestTranslateExcel(unittest.TestCase):
     def test_nonexistent_excel_file(self):
-        excel_file_path = "nonexistent_file.xlsx"
+        excel_file_path = "tests/nonexistent_file.xlsx"
         to_language = "en"
         result = translate_excel(excel_file_path, to_language)
         self.assertIsNone(
@@ -19,18 +19,14 @@ class TestTranslateExcel(unittest.TestCase):
         )
 
     def test_empty_excel_file(self):
-        excel_file_path = (
-            "test_excels/empty_file.xlsx"  # Provide an empty Excel file path
-        )
+        excel_file_path = "tests/test_excels/empty_file.xlsx"
         to_language = "en"
         result = translate_excel(excel_file_path, to_language)
         self.assertIsNone(result, "Expected None for an empty Excel file")
 
     def test_translation_without_export(self):
         expected_df = pd.DataFrame(["Turkish"], columns=["table"])
-        excel_file_path = (
-            "test_excels/sample.xlsx"  # Provide a sample Excel file path
-        )
+        excel_file_path = "tests/test_excels/sample.xlsx"
         to_language = "en"
         result = translate_excel(excel_file_path, to_language)
         print(result)
@@ -44,14 +40,12 @@ class TestTranslateExcel(unittest.TestCase):
         )
 
     def test_translation_with_export(self):
-        excel_file_path = (
-            "test_excels/sample.xlsx"  # Provide a sample Excel file path
-        )
+        excel_file_path = "tests/test_excels/sample.xlsx"
         to_language = "en"
         result = translate_excel(excel_file_path, to_language, export=True)
         self.assertIsNone(result, "Expected None when exporting")
-        self.assertFalse(
-            os.path.isfile("test_excels/sample_translated.xlsx"),
+        self.assertTrue(
+            os.path.isfile("tests/test_excels/sample_translated.xlsx"),
             "File created successfully",
         )
 
