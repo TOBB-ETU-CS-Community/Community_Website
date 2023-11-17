cbe0b0cdd530720f789587cf8b16b531fd07431f
fotino21 <ataturhan21@gmail.com>
2023-11-17 15:20:04 +0300

    config: menu page removed

--- community_website/pages/Programs.py
+++ community_website/pages/Programs.py
@@ -86,7 +86,6 @@ def main():
             Page(
                 "community_website/pages/Programs.py", _("Youth Programs"), "🌟"
             ),
-            Page("community_website/pages/Menu.py", _("Biweekly Menu"), "🍽️"),
             Page(
                 "community_website/pages/Feedback.py", _("Feedback Form"), "📝"
             ),
